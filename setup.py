1d89e81407a2e786b28c39327d5d0da526cbca39
ryan-snyder <spocksbeard16@gmail.com>
2022-03-30 05:16:01 +0000

    Adding proper info to setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='gym_wordle',
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(name='alt_gym_wordle',
     version='0.0.1',
+    author='Ryan Snyder',
+    description='OpenAI gym environment for training agents on Wordle',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ryan-snyder/gym-wordle',
+    packages=find_packages(
+        include=[
+            'gym_wordle',
+            'gym_wordle.*'
+        ]
+    ),
     install_requires=['gym', 'pandas', 'pygame']  # And any other dependencies foo needs
 )
