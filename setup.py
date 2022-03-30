c3b9a5f42ab562424046bebb5f35894df9ad4015
ryan-snyder <spocksbeard16@gmail.com>
2022-03-30 06:14:06 +0000

    feat: added version.py file

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(name='alt_gym_wordle',
-    version='0.1.0',
+    version=get_version('gym_wordle/_version.py'),
     author='Ryan Snyder',
     description='OpenAI gym environment for training agents on Wordle',
     long_description=long_description,
