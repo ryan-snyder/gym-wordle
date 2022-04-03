74c2bb670794da62f07a5a9059fcd0cffd083879
ryan-snyder <36665494+ryan-snyder@users.noreply.github.com>
2022-04-03 05:58:34 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(name='alt_gym_wordle',
-    version='0.2.0',
+    version='0.2.2',
     author='Ryan Snyder',
     description='OpenAI gym environment for training agents on Wordle',
     long_description=long_description,
